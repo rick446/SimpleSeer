755e014e87acf88b43bae2fabdf8290b14680ef0
Anthony Oliver <anthony@ingenuitas.com>
2011-10-19 12:31:14 -0400

    Updated javascript bug

--- SimpleSeer/public/js/poll.js
+++ SimpleSeer/public/js/poll.js
@@ -1,19 +1,22 @@
+var poll = function poll(){
+    $.ajax(
+        {
+        url: "poll",
+        success: function(data){
+                    console.log("check redis");
+                },
+        error: function(data){
+                    console.log("polling error occured");
+                },
+        complete: function(data){
+                    console.log("polling complete");
+                    //~ setInterval(poll, 2000);
+                }
+        }
+    );
+}
+
 $(document).ready(
-    function poll(){
-        $.ajax(
-            {
-            url: "poll",
-            success: function(data){
-                        console.log("check redis");
-                    },
-            error: function(data){
-                        console.log("polling error occured");
-                    },
-            complete: function(data){
-                        console.log("polling complete");
-                        setInterval(poll, 2000);
-                    }
-            }
-        );
-    }
+    window.setInterval(poll, 2000)
 );
+
