44a365cb2ba210a9beeef2cd901e4a7f1035dc9f
Nathan Oostendorp <nate@oostendorp.net>
2011-12-30 09:11:13 -0500

    inching towards a working processing.js interface

--- public/js/simpleseer.js
+++ public/js/simpleseer.js
@@ -1,9 +1,12 @@
 
 /* Make sure the display is always relevant to the browser size
  * parts cribbed from http://jsfiddle.net/gaby/3YLQf/ */
+
+SS = SimpleSeer = new Object();
+
 $(function(){
 
-    var stretcher = $('#maindisplay > img'),
+    var stretcher = $('#display_0'),
         element = stretcher[0],
         currentSize = { width: 0, height: 0 },
         $document = $(document);
@@ -30,7 +33,6 @@ $(function(){
 });
 
 
-SimpleSeer = new Object();
 SimpleSeer.getValue = function(key) {
     returndata = $.parseJSON(
         $.ajax(
@@ -50,6 +52,7 @@ SimpleSeer.framecount = SimpleSeer.getValue('framecount');
 SimpleSeer.poll_interval = parseFloat(SimpleSeer.getValue('poll_interval'));
 
 
+/*
 setInterval(function(){
    thisframe = SimpleSeer.getValue('framecount');
    if (SimpleSeer.framecount != thisframe) {
@@ -59,9 +62,25 @@ setInterval(function(){
    SimpleSeer.framecount = thisframe;
 
 }, SimpleSeer.poll_interval * 1000);
+ */
 
-$(document).ready($('#maindisplay > img').click( function(){
-  alert('hi');
-    
-    
-}))
+SS.p = new Processing('display_0');
+//coloquially, we'll probably always refer to this as SS.p
+
+//this is the "main" loop of the processing app
+
+SS.p.setup = function() {
+  SS.p.size(SS.width, SS.height);
+}
+
+SS.p.draw = function() {
+  SS.p.background(51);   
+  SS.p.fill(255, 80);  
+  SS.p.rect(SS.p.mouseX, SS.p.mouseY, 20, 20);  
+ }
+
+//this executes at document.ready
+SimpleSeer.setup = function(){
+    SS.p.setup();
+    SS.p.loop();
+}
