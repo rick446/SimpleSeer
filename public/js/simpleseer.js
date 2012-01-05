9ac09383db62b8c10b08f383bf349f258638c443
oostendo <nate@oostendorp.net>
2012-01-05 17:10:48 -0500

    radial menu in. pretty dang sexy, now to make the buttons wor

--- public/js/simpleseer.js
+++ public/js/simpleseer.js
@@ -50,11 +50,18 @@ SimpleSeer.getValue = function(key) {
     return returndata['GET'];
 };
 
+//math helper functions
+SimpleSeer.euclidean = function(pt1, pt2) {
+    return Math.sqrt(Math.pow(pt1[0] - pt2[0], 2) + Math.pow(pt1[1] - pt2[1], 2));
+}
+
+
 //import some context from webdis
 SimpleSeer.cameras = SimpleSeer.getValue('cameras');
 SimpleSeer.framecount = SimpleSeer.getValue('framecount');
 SimpleSeer.framedata = [$.parseJSON(SimpleSeer.getValue('currentframedata_0'))];
 SimpleSeer.poll_interval = parseFloat(SimpleSeer.getValue('poll_interval'));
+SimpleSeer.radialAnimating = false;
 
 /* //check the frame id, if it increments, reload context
 setInterval(function(){
@@ -120,7 +127,7 @@ SS.taskhandler = {
             
             SS.p.fill(255, 20);
             SS.p.rect(startx, starty, w, h);
-        }
+        },
         
         manipulate_onclick: function() {
             
@@ -142,68 +149,123 @@ SS.taskhandler = {
             
         }
     }
+}
+
+
+SS.launchRadial = function() {
     
     
+    if (SS.radialAnimating) {
+        return;
+    }
+    oldtask = SS.action["task"];
     
-    
+    if (oldtask == "radial_select") {
+        distance = SS.euclidean(SS.action["startpx"], [SS.mouseX, SS.mouseY]);
+        if (distance > 75) { //TODO we should really change this based on real px, not image pics
+            SS.radialAnimating = true;
+            $("#radial_container").animate( {
+                top: (SS.p.mouseY - 110).toString() + "px", 
+                left: (SS.p.mouseX - 110).toString() + "px" }, 500,
+                function() { SS.radialAnimating = false; } 
+            );    
+        } else {
+            $("#radial_container").css( {
+                top: (SS.p.mouseY - 110).toString() + "px", 
+                left: (SS.p.mouseX - 110).toString() + "px" 
+            });   
+        }   
+    } else {
+        $("#radial_container").radmenu("show").css( { zIndex: 99,
+            top: (SS.p.mouseY - 110).toString() + "px", 
+            left: (SS.p.mouseX - 110).toString() + "px" } );
+    }
+
+
+    SS.action = { startpx: [SS.mouseX, SS.mouseY], task: "radial_select" };
     
 }
+
 
 SS.p.draw = function() {
   SS.setscale();
   SS.p.background(0, 0);     
-  
-  
-  if (SS.action["task"]) {
+    
+  if (SS.action["task"] && !SS.action["task"] == "radial_select") {
       task = SS.action["task"];
       
       if (task in SS.taskhandler) {
-          SS.taskhandler[task].manipulate();
+          if (SS.mouseDown) {
+              SS.taskhandler[task].manipulate_onclick();
+          } else {
+              SS.taskhandler[task].manipulate();
+          } 
+          //or manipulate onclick
       }
-      
-      
-      
+  } else {
+      if (SS.mouseDown) {
+          SS.launchRadial();
+      }
   }
   
   //SS.p.fill(255, 20);  
   //SS.p.rect(SS.mouseX, SS.mouseY, 20, 20);  
  }
 
- 
 SS.p.mousePressed = function() {
-  //SS.setscale();
-  
-    if (!SS.action["task"]) {
-        SS.action = { startpx: [SS.mouseX, SS.mouseY], task: "radial_select" };
-        $("#radial").radmenu("show").css( { zIndex: 99, top: SS.p.mouseY.toString() + "px", left: SS.p.mouseX.toString() + "px" } );
+    SS.launchRadial();
+    if (SS.action["task"] && !SS.action["task"] == "radial_select") {
+      task = SS.action["task"];
+      
+      if (task in SS.taskhandler) {
+          SS.taskhandler[task].manipulate_onclick();
+          //or manipulate onclick
+      }
     } else {
-        if (task in SS.taskhandler) {
-            SS.taskhandler[task].manipulate_onclick();
-        }
+      SS.launchRadial();
     }
-}
+} 
+
 
 //this executes at document.ready
 SimpleSeer.setup = function(){
     SS.p.setup();
     SS.p.loop();
+
+/* these functions just give us a little extra context for when processing doesn't pick up events*/
+   $("#maindisplay").mousedown( function(e) {
+      SS.mouseDown = true;
+   });
+
+   $("#maindisplay").mouseup( function(e) {
+      SS.mouseDown = false; 
+   });
+
+   $("#maindisplay").mousemove(function(e) {
+  //SS.setscale();
+    SS.p.mouseX = e.pageX - $("#display").offset()["left"];
+    SS.p.mouseY = e.pageY - $("#display").offset()["top"];
+   });
+/*end processing helpers*/
+
+   $(".radialitem > a").mouseenter( function(e) {
+      alert("hi");
+       
+   });
     
-    $("#radial").radmenu({
-        listClass: 'radial_items', // the list class to look within for items
-        itemClass: 'radial_item', // the items - NOTE: the HTML inside the item is copied into the menu item
-        radius: 100, // radius in pixels
-        animSpeed:400, // animation speed in millis
-        centerX: -10, // the center x axis offset
-        centerY: -10, // the center y axis offset
+
+   $("#radial_container").radmenu({
+        listClass: 'radiallist', // the list class to look within for items
+        itemClass: 'radialitem', // the items - NOTE: the HTML inside the item is copied into the menu item
+        radius: 68, // radius in pixels
+        centerX: 10, // the center x axis offset
+        centerY: -6, // the center y axis offset
         selectEvent: "click", // the select event (click)
         onSelect: function($selected){ // show what is returned 
-            id = $selected.children()[0].id;
-            task = id.substr(12); //get the task from the id
-            SS.action["task"] = task;
-        
-            $("#radial").radmenu("hide").css( { zIndex: -1, top: "0px", left: "0px" } );
-
+          alert("you clicked on .. " + $selected.index());
         },
-        angleOffset: 0 // in degrees
-    });
+        onShow: function($items){$items.show();$('#radial_container').fadeIn(500);},
+        onHide: function($items){$items.hide();$('#radial_container').hide();}
+      });
+
 }
