63cebbd15aabc23bb42ece6694cfd9a54b0160a0
Nathan Oostendorp <nate@oostendorp.net>
2012-05-17 22:06:54 -0400

    Result is a first-class model, so they say

--- SimpleSeer/models/__init__.py
+++ SimpleSeer/models/__init__.py
@@ -3,6 +3,7 @@ from FrameFeature import FrameFeature
 from Inspection import Inspection, InspectionSchema
 from Measurement import Measurement, MeasurementSchema
 from OLAP import OLAP, OLAPSchema
+from Result import Result
 from Watcher import Watcher
 # from Statistic import Statistic
 
