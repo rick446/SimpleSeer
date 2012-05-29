9952f0808a6e131f14128d1121450fdbe2297862
Katherine Scott <katherineAScott@gmail.com>
2012-05-29 14:40:31 -0400

    got region ready to test

--- SimpleSeer/plugins/blob/blob.py
+++ SimpleSeer/plugins/blob/blob.py
@@ -21,39 +21,146 @@ plugin this, blob:Blob
     def __call__(self, image):
         params = util.utf8convert(self.inspection.parameters)
 
-        #if we have a color parameter, lets threshold
-        blobs = []
+        # okay I am going to gut this, so it will break stuff.
+        # the new method allows the user to use one of the following methods
+        #
+        # Hue - find things with a certain color
+        # Color - find things with a certain color
+        # Raw - binary adaptive or blanket threshold - old school blobs
+        # FloodFill - pick a pixel and a tolerance - find blob from there 
+        #  
+        # eventually all of the location based approaches should support multiple locations
+        # to sample from 
+        #
+        # For hue and a raw the user needs to specify a single upper 
+        # and lower bound variable. 
+        # 
+        # For color and flood fill the user can specify both the
+        # upper and lower color bound as two precise triplets
+        # or as a single triplet as the delta around the color
+        # The suggested UI for this 
+        # is a color palette in gimp where the user can select a region 
+        # around the value ... N.B. eventually we should actually render
+        # this stuff in 3D and let people pare down the color cube/cone 
+        #
+        # The user also has the option to invert the results, and 
+        # set the minimum and max size of the blob.
+        #
+        # If the user provides parameters for multiple of 
+        # the above approaches we return None. 
+
         invert = False
-        pixel = ""
+        
+        hue = None
+        hueLocation = None
+        doHue = False
+        color = None
+        colorLocation = None
+        doColor = False
+        location = None
+        doFF = True 
+        thresh1 = None
+        thresh2 = None
+        
+        parse_error = False
+
         if params.has_key("invert"):
             invert = params["invert"]
             del params["invert"]
-
-        if params.has_key("pixel"):
-            pixel = params['pixel'].split(",")
-            del params["pixel"]
-
-        if params.has_key("hue"):
-            image = image.hueDistance(int(params["hue"])).invert()
+        
+        #do the parsing for hue distance 
+        if( params.has_key("hueLocation")): # I am assuming this is the (x,y) pos of a reference pixel
+            hueLoc = tuple(int(item) for item in params['hueLocation'].strip("()").split(","))
+            del params["hueLocation"]
+            hue = image[hueLoc[0],hueLoc[1]]
+            # hue = Color.BGRToHue(hue)
+            doHue = True
+        elif( params.hasKey("hue")):
+            hue = int(params['hue'])
             del params["hue"]
+            doHue = True
 
-        if pixel:
-            color = str(image.getPixel(int(pixel[0]), int(pixel[1])))[1:-1]
-            params["color"] = color
-            self.inspection.parameters["color"] = color
-            del self.inspection.parameters["pixel"]
-            if self.inspection.id:
-                self.inspection.save()
-
-        if params.has_key("color"):
-            r,g,b = params["color"].split(",")
-            image = image.colorDistance((float(r), float(g), float(b))).invert()
+        if( params.has_key("colorLocation")): # I am assuming this is the (x,y) pos of a reference pixel
+            colorLoc = tuple(int(item) for item in params['colorLocation'].strip("()").split(","))
+            color = image[colorLoc[0],colorLoc[1]]            
+            del params["colorLocation"]
+            doColor = True
+        elif( params.hasKey("color")):
+            color = params['color'].split(",")
             del params["color"]
+            doColor = True
 
-        if invert:
-            blobs = image.invert().findBlobs(**params)
-        else:
-            blobs = image.findBlobs(**params)
+        if( params.has_key("location")): # I am assuming this is the (x,y) pos of a reference pixel
+            location = tuple(int(item) for item in params['location'].strip("()").split(","))
+            del params["location"]
+            doFF = True
+
+        #These are for fine grain control of flood fill
+        #We can also use them to pick out colors using the createBinaryMask function. 
+        if( params.has_key("thresh1") ):
+            thresh1 = tuple(int(item) for item in params['thresh1'].strip("()").split(","))
+            del params["thresh1"]
+
+        if( params.has_key("thresh2") ):
+            thresh1 = tuple(int(item) for item in params['thresh2'].strip("()").split(","))
+            del params["thresh2"]
+
+
+        #now get the other params
+        thresh=-1
+        blocksize=0
+        p=5
+        minsize = 10
+        maxsize = 0
+        if params.has_key("thresh"):
+            thresh = params["thresh"]
+            del params["thresh"]
+        if params.has_key("blocksize"):
+            blocksize = params["blocksize"]
+            del params["blocksize"]
+        if params.has_key("p"):
+            p = params["p"]
+            del params["p"]
+
+                
+        count = 0
+        if( doFF ):
+            count = count + 1
+            if( thresh1 is None ):
+                count = count + 1
+        if( doHue ):
+            count = count + 1
+        if( doColor ):
+            count = count + 1
+
+        if( count > 1 ): # user gets to specify one operation - if they select two error out
+            return []
+        
+        if( doHue ):#hue distance
+            mask = image.hueDistance(hue)
+            mask = mask.binarize(thresh=thresh,blocksize=blocksize,p=p)
+            # there is a lot more room to do cool stuff in here
+        elif( doColor ): #color distance
+            mask = image.colorDistance(color)
+            mask = mask.binarize(thresh=thresh,blocksize=blocksize,p=p)
+            # there is a lot more room to do cool stuff in here
+        elif( doFF ):
+            #eventually we could pepper an ROI with spots to get more robust results. 
+            if( thresh2 is not None):
+                mask = image.floodFillToMask(location,lower=thresh1,upper=thresh2)
+            else:
+                mask = image.floodFillToMask(location,tolerance=thresh1)
+        else: #vanilla binarize
+            mask = img.binarize(thresh=thresh,blocksize=blocksize,p=p)
+        # SUGGEST ALSO USING CREATE BINARY MASK HERE 
+        # WE COULD ALSO AUTOGEN MASK USING THE FIND BLOBS FROM PALLETTE IF YOU WANT TO GET FANCY
+
+        if( invert ):
+            mask = mask.invert()
+
+        # SUGGEST ADDING AN OPTIONAL DILATE / ERODE HERE
+        
+        blobs = image.findBlobsFromMask(mask,minsize=minsize,maxsize=maxsize)
 
         if not blobs:
             return []
--- SimpleSeer/plugins/region/region.py
+++ SimpleSeer/plugins/region/region.py
@@ -1,36 +1,63 @@
 import SimpleCV
-
 from SimpleSeer import util
 from SimpleSeer import models as M
 
 from SimpleSeer.plugins import base
 
-class RegionFeature(SimpleCV.Feature):
+# right now this will act just like a blob the interface should be exactly the same  
+class RegionFeature(SimpleCV.Blob):
 
-    def __init__(self, image, startx, starty, width, height):
-
-        point = (startx, starty)
-        self.x = point[0] + width / 2
-        self.y = point[1] + height / 2
-        self.points = [tuple(point),
-            (point[0] + width, point[1]),
-            (point[0] + width, point[1] + height),
-            (point[0], point[1] + height)]
-
-        self.image = image
-
-    def meanColor(self):
-        return self.crop().meanColor()
+    def __init__(self, blob):
+        self = blob
 
 #below are "core" inspection functions
 class Region(base.InspectionPlugin):
 
     def __call__(self, image):
         params = util.utf8convert(self.inspection.parameters)
+        retVal = []
+        mask = Image((image.width,image.height))
+        if( params.hasKey('w') and  params.hasKey('h') and params.hasKey('x') and params.hasKey('y') ): #rectangle
+            if( params['x'] + params['w'] < image.width and
+                params['y'] + params['h'] < image.height and
+                params['y'] >= 0 and 
+                params['x'] >= 0 ):
+                mask.drawRectangle(params['x'],params['y'],params['w'],params['h'],width=-1,color=Color.WHITE)
+                mask = mask.applyLayers()
+                fs = image.findBlobsFromMask(mask)
+                ff = M.FrameFeature()
+                if( fs is not None ):                    
+                    ff.setFeature(RegionFeature(fs[-1])) # a little hacky but I am sure that it works
+                    retVal = [ff]
+  
+        elif( params.hasKey('x') and  params.hasKey('y') and params.hasKey('r') ): # circle
+            if( params['x'] + params['r'] < image.width and
+                params['y'] + params['r'] < image.height and 
+                params['x'] - params['r'] >= 0 and 
+                params['y'] - params['r'] >= 0 ):
 
-        if params['x'] + params['w'] > image.width or params['y'] + params['h'] > image.height:
-            return []
+                r = params['r']
+                x = params['x']
+                y = params['y']
 
-        ff = M.FrameFeature()
-        ff.setFeature(RegionFeature(image, params['x'], params['y'], params['w'], params['h']))
-        return [ff]
+                mask.drawCircle((x,y),r,thickness=-1,color=Color.WHITE)
+                mask = mask.applyLayers()
+                fs = image.findBlobsFromMask(mask)
+                ff = M.FrameFeature()
+                if( fs is not None ):                    
+                    ff.setFeature(RegionFeature(fs[-1]))
+                    retVal = [ff]
+
+        elif( params.hasKey('contour') ):
+            contour = params['contour']
+            if( len(contour) >=  3 ):
+                mask.dl().polygon(contour,filled=True,color=Color.WHITE)
+                mask = mask.applyLayers()
+                fs = image.findBlobsFromMask(mask)
+                ff = M.FrameFeature()
+                if( fs is not None ):                    
+                    ff.setFeature(RegionFeature(fs[-1]))
+                    retVal = [ff]
+
+
+        return retVal
