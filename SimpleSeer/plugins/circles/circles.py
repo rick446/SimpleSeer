9870933715f3fb4d34843aa7f95c9a9c611c85dd
Katherine Scott <katherineAScott@gmail.com>
2012-05-29 16:40:52 -0400

    Okay circles should be good

--- SimpleSeer/plugins/circles/circles.py
+++ SimpleSeer/plugins/circles/circles.py
@@ -5,48 +5,74 @@ from SimpleSeer import models as M
 from SimpleSeer import util
 
 from SimpleSeer.plugins import base
-
 """
-Overly simplified motion detection plugin
-insp = Inspection( name= "Motion", method="motion", camera = "Default Camera")
+Overly simplified region detection plugin
+
+insp = Inspection( name= "Circles", 
+                   method="corc;es", 
+                   camera = "Default Camera")
 insp.save()
 
-meas = Measurement( name="movement", label="Movement", method = "movement", parameters = dict(), units = "", featurecriteria = dict( index = 0 ), inspection = insp.id )
+
+meas = Measurement( name="radius", 
+                    label="radius", #Human readable name 
+                    method = "radius", #the method to call on a regionFeature
+                    parameters = dict(), #not needed - store parameters here
+                    units = "pixels", # 
+                    featurecriteria = dict( index = 0 ), #not used
+                    inspection = insp.id #point back to the original inspection)
+
 meas.save()
 
 
 """
 
-class MotionFeature(SimpleCV.Feature):
-  movement = 0.0
-
-  def __init__(self, image, mval, top = 0, left = 0, right = -1, bottom = -1):
-    #TODO, if parameters are given, crop on both images
-    self.image = image
-    self.movement = mval
-
-    if (right == -1):
-      right = image.width
-
-    if (bottom == -1):
-      bottom = image.height
-
-    self.points = [(left, top), (right, top), (right, bottom), (left, bottom)]
-    self.x = left + self.width() / 2
-    self.y = top + self.height() / 2
-
-class Motion(base.InspectionPlugin):
-
+class Circle(base.InspectionPlugin):
+  """
+  line parameters 
+  
+  extraction parameters:
+  length = (minlength,maxlength)
+  angle = (minangle,maxangle)
+  canny = (lower,upper,threshold) 
+  gap = line gap 
+  """
   def __call__(self, image):
-    SS = util.get_seer()
-    if len(SS.lastframes) > 1:
-      #TODO, find the index of the named camera
-      lastimage = SS.lastframes[-2][0].image
-    else:
-      return None
+    params = util.utf8convert(self.inspection.parameters)
+    
+    # It is just going to be easier to mask over 
+    # all of the default params. 
+    canny = 100 
+    threshold = 350
+    distance = -1
+    radius = (None,None)
 
-    diff = (image - lastimage) + (lastimage - image)
+    retVal = []
 
-    ff = M.FrameFeature()
-    ff.setFeature(MotionFeature(image, np.mean(diff.meanColor())))
-    return [ff]
+    #we assume all of this is validated and correct 
+    if( params.hasKey('canny') ):
+      canny = params('canny') 
+
+    if( params.hasKey('distance') ):
+      gap = params('distance') 
+
+    if( params.hasKey('radius') ):
+      angle = params('radius') 
+
+    if( params.hasKey('threshold') ):
+      angle = params('threshold') 
+      
+    fs = img.findCircles(canny,threshold,distance)
+
+    if fs is not None:
+      if( angle[0] is not None):
+        fs = FeatureSet(f for f in fs if( f.radius()>=radius[0] ) )
+      if( angle[1] is not None ):
+        fs = FeatureSet(f for f in fs if( f.radius()<=radius[1] ) )
+      fs.draw()
+      for f in fs: # do the conversion from SCV featureset to SimpleSeer featureset
+        ff = M.FrameFeature()
+        ff.setFeature(f)
+        retVal.append(ff)
+
+    return retVal 
