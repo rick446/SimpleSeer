e6f82702ed641b6894af2285e528197ccf9e48ca
Katherine Scott <katherineAScott@gmail.com>
2012-05-31 14:26:16 -0400

    added edge feature widths, requires a pull from SimpleCV master

--- SimpleSeer/plugins/edgeWidth/edgeWidth.py
+++ SimpleSeer/plugins/edgeWidth/edgeWidth.py
@@ -8,7 +8,7 @@ from SimpleSeer.plugins import base
 
 """
 Overly simplified motion detection plugin
-insp = Inspection( name= "Motion", method="motion", camera = "Default Camera")
+insp = Inspection( name= "EdgeWidth", method="edgeWidth", camera = "Default Camera")
 insp.save()
 
 meas = Measurement( name="movement", label="Movement", method = "movement", parameters = dict(), units = "", featurecriteria = dict( index = 0 ), inspection = insp.id )
@@ -17,36 +17,64 @@ meas.save()
 
 """
 
-class MotionFeature(SimpleCV.Feature):
-  movement = 0.0
+class EdgeWidthFeature(SimpleCV.Line):
+  distance = 0 #the distance between the two intersection points
+  intersectionPts = [] # the two points of intersection
+  sourceLinePts = [] # the two points that define the line where we look for intersections
 
-  def __init__(self, image, mval, top = 0, left = 0, right = -1, bottom = -1):
+  def __init__(self, image, srcPts,intersections ):
     #TODO, if parameters are given, crop on both images
     self.image = image
-    self.movement = mval
-
-    if (right == -1):
-      right = image.width
-
-    if (bottom == -1):
-      bottom = image.height
-
-    self.points = [(left, top), (right, top), (right, bottom), (left, bottom)]
-    self.x = left + self.width() / 2
-    self.y = top + self.height() / 2
-
-class Motion(base.InspectionPlugin):
+    self.end_points = intersections
+    self.sourceLinePts = srcPts 
+    line = intersections 
+    at_x = (line[0][0] + line[1][0]) / 2
+    at_y = (line[0][1] + line[1][1]) / 2
+    xmin = np.min([line[0][0],line[1][0]])
+    xmax = np.max([line[0][0],line[1][0]])
+    ymax = np.min([line[0][1],line[1][1]])
+    ymin = np.max([line[0][1],line[1][1]])
+    points = [(xmin,ymin),(xmin,ymax),(xmax,ymax),(xmax,ymin)]
+    super(Line, self).__init__(i, at_x, at_y,points)
+    
+class EdgeWidth(base.InspectionPlugin):
 
   def __call__(self, image):
-    SS = util.get_seer()
-    if len(SS.lastframes) > 1:
-      #TODO, find the index of the named camera
-      lastimage = SS.lastframes[-2][0].image
-    else:
-      return None
+    pt0 = None
+    pt1 = None 
+    canny1 = 0
+    canny2 = 100 
+    width = 3
 
-    diff = (image - lastimage) + (lastimage - image)
+    params = util.utf8convert(self.inspection.parameters)    
 
-    ff = M.FrameFeature()
-    ff.setFeature(MotionFeature(image, np.mean(diff.meanColor())))
-    return [ff]
+    if( params.has_key("pt0")):
+      pt0 = params["pt0"]
+    else: # required
+      return []
+
+    if( params.has_key("pt1")):
+      pt1 = params["pt1"]
+    else: # required
+      return []
+
+    if( params.has_key("canny2")):
+      canny2 = params["canny2"]
+      
+    if( params.has_key("canny1")):
+      canny1 = params["canny1"]
+
+    if( params.has_key("width")):
+      width = params["width"]
+  
+    result = image.edgeIntersections(pt0,pt1,width,canny1,canny2)
+    
+    retVal = []
+    if(result[0] is not None and
+       result[1] is not None ):
+      ff = M.FrameFeature()
+      ewf = EdgeWidthFeature(image,[pt0,pt1],result)
+      ff.setFeature(ewf)
+      retVal.append(ff)
+      
+    return retVal
