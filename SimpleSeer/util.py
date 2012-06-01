c643156b646c4b603c84686af2300553c8091bad
Rick Copeland <rick@arborian.com>
2012-06-01 17:39:00 -0400

    Add a lazily-calculated and saved thumbnail property to frames
    Signed-off-by: Rick Copeland <rick@arborian.com>

--- SimpleSeer/util.py
+++ SimpleSeer/util.py
@@ -7,6 +7,18 @@ from decorator import decorator
 from base import jsonencode
 import mongoengine
 
+class LazyProperty(object):
+
+    def __init__(self, func):
+        self._func = func
+        self.__name__ = func.__name__
+        self.__doc__ = func.__doc__
+
+    def __get__(self, obj, klass=None):
+        if obj is None: return None
+        result = obj.__dict__[self.__name__] = self._func(obj)
+        return result
+
 def jsonify(f):
     """
     We're going to use our own jsonify decorator, which uses our jsonencode function
