64b8451ef2f9092b629bfd3e51a1bbe1c887f8cd
Tendrid <tendrid@gmail.com>
2012-06-05 16:33:16 -0400

    simplify FrameSet for now

--- SimpleSeer/models/FrameSet.py
+++ SimpleSeer/models/FrameSet.py
@@ -11,26 +11,19 @@ from SimpleSeer import util
 
 
 from .base import SimpleDoc, WithPlugins
-from .Measurement import Measurement
-from .FrameFeature import FrameFeature
+#from .Measurement import Measurement
+#from .FrameFeature import FrameFeature
+from .Frame import Frame
 
 
 class FrameSetSchema(fes.Schema):
     name = fev.UnicodeString(not_empty=True)
-    frames = V.JSON(if_empty=dict, if_missing=None)
-    #parent = V.ObjectId(if_empty=None, if_missing=None)
-    #method = fev.UnicodeString(not_empty=True)
-    #camera = fev.UnicodeString(if_empty="")
-    #parameters = V.JSON(if_empty=dict, if_missing=None)
-    #filters = V.JSON(if_empty=dict, if_missing=None)
-    #richattributes = V.JSON(if_empty=dict, if_missing=None)
-    #morphs = fe.ForEach(fev.UnicodeString(), convert_to_list=True)
-
-
+    frames = fe.ForEach(fev.UnicodeString(), convert_to_list=True)
 
 class FrameSet(SimpleDoc, WithPlugins, mongoengine.Document):
     name = mongoengine.StringField()
-    frames = mongoengine.ReferenceField('Frame')
+    #frames = mongoengine.ListField(mongoengine.ReferenceField('Frame'))
+    frames = mongoengine.ListField(mongoengine.StringField())
 
     def __repr__(self):
       return "[%s Object <%s> ]" % (self.__class__.__name__, self.name)
