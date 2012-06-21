6d6e3eb06c25932d5e6e00faad840523352ee463
Kurt DeMaagd <kurt@ingenuitas.com>
2012-06-21 17:57:18 -0400

    move Result to the new Realtime OLAP

--- SimpleSeer/models/Result.py
+++ SimpleSeer/models/Result.py
@@ -33,7 +33,8 @@ class Result(SimpleDoc, mongoengine.Document):
     measurement = mongoengine.ObjectIdField()
     
     meta = {
-        'indexes': ["capturetime", ('camera', '-capturetime'), "frame", "inspection", "measurement"]
+        'indexes': ["capturetime", ('camera', '-capturetime'), "frame", "inspection", "measurement", 
+                    ('measurement', '-capturetime'), ('inspection', '-capturetime')]
     }
 
     
@@ -41,17 +42,15 @@ class Result(SimpleDoc, mongoengine.Document):
         # Shortcut to return the capture time in epoch microseconds
         
         epochTime = calendar.timegm(self.capturetime.timetuple()) + self.capturetime.time().microsecond / 1000000.0
-        # Quick hack for jim: may change this:
-        # epochTime *= 1000
         return epochTime
 
     capturetimeEpochMS = property(capEpochMS)
 
     def save(self, *args, **kwargs):
         # Push notification to OLAP to decide whether to publish this update
-        from .OLAP import RealtimeOLAP
-        o = RealtimeOLAP()
-        o.realtime(self)
+        from SimpleSeer.OLAPUtils import RealtimeOLAP
+        ro = RealtimeOLAP()
+        ro.realtime(self)
         
         super(Result, self).save(*args, **kwargs)
 
