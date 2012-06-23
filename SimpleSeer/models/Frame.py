7599e9541bd517b8d9d290344a50692da610c5da
Nate Oostendorp <nate@sightmachine.com>
2012-06-22 23:28:29 -0400

    i hate geanys indentation crap

--- SimpleSeer/models/Frame.py
+++ SimpleSeer/models/Frame.py
@@ -61,7 +61,7 @@ class Frame(SimpleDoc, mongoengine.Document):
             img = self.image
             thumbnail_img = img.scale(140.0 / img.height)
             if self.id and not is_slave in Session().mongo:
-			    img_data = StringIO()
+                img_data = StringIO()
                 thumbnail_img.save(img_data, "jpeg", quality = 25)
                 self.thumbnail_file.put(img_data.getvalue(), content_type='image/jpeg')
                 self.save()
