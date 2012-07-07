af131786b795b6108c0f719e6e5af3415cd9594c
Nate Oostendorp <nate@sightmachine.com>
2012-07-06 20:16:11 -0400

    make sure we don't clobber stuff

--- setup.py
+++ setup.py
@@ -18,10 +18,7 @@ notebook = SimpleSeer.commands:NotebookCommand
 controls = SimpleSeer.commands:ControlsCommand
 replicate = SimpleSeer.commands:ReplicateCommand
 '''
-with open(plugins_ini) as fp:
-    entry_points += fp.read()
-
-entry_points = '''
+entry_points += '''
 [paste.paster_create_template]
 simpleseer=SimpleSeer.template:SimpleSeerProjectTemplate
 '''
